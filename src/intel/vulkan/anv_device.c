3553440e8732ca532e2d8bd161c5b69ff9ca543a
Jianxun Zhang <jianxun.zhang@intel.com>
2024-09-01 12:56:42 -0700

    anv: Use dedicated allocation on modifiers of discrete GPUs (xe2)
    With dedicated allocation information, we can tell an image that
    requires scanout flag needed by its modifier on discrete platforms.
    
    Scanout flag must be set for kernel driver to allocate a continuous
    memory region.
    
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -1665,26 +1665,43 @@ VkResult anv_AllocateMemory(
 
    /* Regular allocate (not importing memory). */
 
+   /* The I915_FORMAT_MOD_4_TILED_BMG_CCS modifier defined in drm_fourcc.h
+    * has a requirement on size:
+    *
+    *    The GEM object must be stored in contiguous memory with a size
+    *    aligned to 64KB.
+    *
+    * Xe kernel driver will provide continuous allocation on scanout buffers
+    * with CCS AND when the bo's size is a multiple of 64KB that is addressed
+    * in isl. Assuming this requirement will be on later discrete GPUs, set
+    * scanout flag on all Xe2+ modifiers.
+    */
+   struct anv_image *image = dedicated_info &&
+                             dedicated_info->image != VK_NULL_HANDLE ?
+                             anv_image_from_handle(dedicated_info->image) :
+                             NULL;
+
+   if (image && device->info->ver >= 20 && device->info->has_local_mem &&
+       isl_drm_modifier_has_aux(image->vk.drm_format_mod)) {
+      alloc_flags |= ANV_BO_ALLOC_SCANOUT;
+   }
+
    result = anv_device_alloc_bo(device, "user", pAllocateInfo->allocationSize,
                                 alloc_flags, client_address, &mem->bo);
    if (result != VK_SUCCESS)
       goto fail;
 
-   if (dedicated_info && dedicated_info->image != VK_NULL_HANDLE) {
-      ANV_FROM_HANDLE(anv_image, image, dedicated_info->image);
-
+   if (image && image->vk.wsi_legacy_scanout) {
       /* Some legacy (non-modifiers) consumers need the tiling to be set on
        * the BO.  In this case, we have a dedicated allocation.
        */
-      if (image->vk.wsi_legacy_scanout) {
-         const struct isl_surf *surf = &image->planes[0].primary_surface.isl;
-         result = anv_device_set_bo_tiling(device, mem->bo,
-                                           surf->row_pitch_B,
-                                           surf->tiling);
-         if (result != VK_SUCCESS) {
-            anv_device_release_bo(device, mem->bo);
-            goto fail;
-         }
+      const struct isl_surf *surf = &image->planes[0].primary_surface.isl;
+      result = anv_device_set_bo_tiling(device, mem->bo,
+                                        surf->row_pitch_B,
+                                        surf->tiling);
+      if (result != VK_SUCCESS) {
+         anv_device_release_bo(device, mem->bo);
+         goto fail;
       }
    }
 

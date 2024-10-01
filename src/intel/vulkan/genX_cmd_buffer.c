3f4e8202f99edd4a894b57c427d92370601ffa06
Rohan Garg <rohan.garg@intel.com>
2024-10-01 10:59:53 +0200

    anv: convert MSAA resolves flushes to resource barriers
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>
    Reviewed-by: Jianxun Zhang <jianxun.zhang@intel.com>

--- src/intel/vulkan/genX_cmd_buffer.c
+++ src/intel/vulkan/genX_cmd_buffer.c
@@ -6170,10 +6170,18 @@ void genX(CmdEndRendering)(
           * the result of writes to the MSAA color attachments show up in the
           * sampler when we blit to the single-sampled resolve target.
           */
+#if GFX_VER >= 20
+         fill_access_mask_for_stage(cmd_buffer,
+                                    VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
+                                    VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT,
+                                    VK_PIPELINE_STAGE_2_RESOLVE_BIT,
+                                    VK_ACCESS_2_SHADER_SAMPLED_READ_BIT);
+#else
          anv_add_pending_pipe_bits(cmd_buffer,
                                    ANV_PIPE_TEXTURE_CACHE_INVALIDATE_BIT |
                                    ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT,
                                    "MSAA resolve");
+#endif
       }
 
       const bool has_depth_resolve =

e7bde8b019ee1d3b016e7092a5a509d51793921a
Karol Herbst <kherbst@redhat.com>
2024-12-16 00:28:22 +0100

    gallium: new VM interfaces for SVM
    The old interfaces added back in clover's time were modeled after a very
    bindful resource model.
    
    However SVM (shared virtual memory) requires us to be way more flexible.
    
    The new interfaces allow frontends to create a cut-out in the GPU's vm and
    to assign addresses themselves. This gives us the following benefits:
     - The frontend is empowered to synchronize resource addresses between
       several devices. cl_mem objects in OpenCL span across a set of multiple
       devices and SVM requires them to have the same VMA across all of them.
     - Coarse grain SVM can be implemented without bothering drivers too much
       as the frontend can be responsible to make sure a host allocation with
       a specific VMA matches a GPU allocation with the identical VMA.
     - Support for Global variables in the CrossWorkgroup storage class
       Initializers. Those can depend on addresses of CrossWorkgroup memory,
       if the frontend can just assign a VMA, this address can be passed as a
       constant to spirv_to_nir and folded without the need to support
       spilling of constant initializers.
    
    Drivers not able to give us a vm-cutout are left with implementing
    cl_ext_buffer_device_address instead.
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/gallium/include/pipe/p_state.h
+++ src/gallium/include/pipe/p_state.h
@@ -1289,6 +1289,15 @@ struct pipe_memory_object
    bool dedicated;
 };
 
+/**
+ * Structure that contains information about a vm allocation
+ */
+struct pipe_vm_allocation
+{
+   uint64_t start;
+   uint64_t size;
+};
+
 #ifdef __cplusplus
 }
 #endif

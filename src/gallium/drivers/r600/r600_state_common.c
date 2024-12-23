Grima04/mesa-turnip-kgsl

commit 0cae8d372e644a1d89ec5685781435159ee2d285

r600: don't set an index_bias for indirect draw calls
The bug #4657 mixed up which commit was actually responsible for
the regression, so re-apply this patch.

The indirect draw call already encodes the index bias so that no
additional encoding in the hardware is needed in this case.

This fixes a regression with a number of tests from
   dEQP-GLES31.functional.draw_indirect.random.*

Fixes: c6c532faa8cf172715b38783ec7175b626a63944
  "gallium/u_vbuf: use updated pipe_draw_start_count while using draw_vbo"

Closes #4671

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10388>

src/gallium/drivers/r600/r600_state_common.c
@@ -2225,7 +2225,7 @@ static void r600_draw_vbo(struct pipe_context *ctx, const struct pipe_draw_info 
 		}
 		index_bias = info->index_bias;
 	} else {
-		index_bias = draws[0].start;
+		index_bias = indirect ? 0 : draws[0].start;
 	}
 
 	/* Set the index offset and primitive restart. */

mirror/mesa

commit f30349169a239e45f1d9a613e85ba7950cb48908

r600: index_bias should be forced to zero for all indirect draw calls
The previous fix 0cae8d372e64 is the right way to proceed, but it
should also apply when index_size is non-zero.

This change was tested on palm and cayman. Here is the test fixed:
spec/arb_multi_draw_indirect/arb_draw_elements_base_vertex-multidrawelements -indirect: fail pass

Fixes: 0cae8d372e64 ("r600: don't set an index_bias for indirect draw calls")
Signed-off-by: Patrick Lerda <patrick9876@free.fr>

src/gallium/drivers/r600/r600_state_common.c
@@ -2264,9 +2264,9 @@ static void r600_draw_vbo(struct pipe_context *ctx, const struct pipe_draw_info 
 			index_offset -= start_offset;
 			has_user_indices = false;
 		}
-		index_bias = draws->index_bias;
+		index_bias = unlikely(indirect) ? 0 : draws->index_bias;
 	} else {
-		index_bias = indirect ? 0 : draws[0].start;
+		index_bias = unlikely(indirect) ? 0 : draws[0].start;
 	}
 
 	/* Set the index offset and primitive restart. */
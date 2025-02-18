6de4ec57078febbd75feadb6c8e8cc62371ae774
Daniel Schürmann <daniel@schuermann.dev>
2025-02-18 11:24:00 +0100

    aco/insert_exec_mask: don't disable dead quads on demote in divergent CF
    Totals from 1507 (1.90% of 79377) affected shaders: (Navi31)
    Instrs: 1533853 -> 1523057 (-0.70%); split: -0.72%, +0.01%
    CodeSize: 8185652 -> 8142980 (-0.52%); split: -0.53%, +0.01%
    Latency: 16504655 -> 16493611 (-0.07%); split: -0.07%, +0.01%
    InvThroughput: 2524668 -> 2524640 (-0.00%); split: -0.00%, +0.00%
    SClause: 39177 -> 39215 (+0.10%); split: -0.13%, +0.22%
    Copies: 97441 -> 94968 (-2.54%); split: -2.73%, +0.20%
    Branches: 31997 -> 31967 (-0.09%); split: -0.12%, +0.03%
    PreSGPRs: 60062 -> 60281 (+0.36%); split: -0.00%, +0.37%
    SALU: 144417 -> 135712 (-6.03%); split: -6.16%, +0.13%

--- src/amd/compiler/aco_insert_exec_mask.cpp
+++ src/amd/compiler/aco_insert_exec_mask.cpp
@@ -495,22 +495,16 @@ process_instructions(exec_ctx& ctx, Block* block, std::vector<aco_ptr<Instructio
             instr->definitions[1] = bld.def(s1, scc);
          }
       } else if (instr->opcode == aco_opcode::p_demote_to_helper) {
+         assert(!ctx.handle_wqm || state == WQM);
          assert((info.exec[0].type & mask_type_exact) && (info.exec[0].type & mask_type_global));
 
-         const bool nested_cf = !(info.exec.back().type & mask_type_global);
-         if (ctx.handle_wqm && state == Exact && nested_cf) {
-            /* Transition back to WQM without extra instruction. */
-            info.exec.pop_back();
-            state = WQM;
-         } else if (block->instructions[idx + 1]->opcode == aco_opcode::p_end_wqm) {
+         if (block->instructions[idx + 1]->opcode == aco_opcode::p_end_wqm) {
             /* Transition to Exact without extra instruction. */
             info.exec.resize(1);
             state = Exact;
-         } else if (nested_cf) {
-            /* Save curent exec temporarily. */
-            info.exec.back().op = bld.copy(bld.def(bld.lm), Operand(exec, bld.lm));
-         } else {
-            info.exec.back().op = Operand(exec, bld.lm);
+         } else if (info.exec.size() == 1) {
+            /* Make sure to not use some previously stored temporary. */
+            info.exec[0].op = Operand(exec, bld.lm);
          }
 
          /* Remove invocations from global exact mask. */
@@ -527,24 +521,11 @@ process_instructions(exec_ctx& ctx, Block* block, std::vector<aco_ptr<Instructio
          bld.insert(std::move(instr));
 
          /* Update global WQM mask and store in exec. */
-         if (state == WQM) {
+         if (state == WQM && (info.exec.back().type & mask_type_global)) {
             assert(info.exec.size() > 1);
             bld.sop1(Builder::s_wqm, Definition(exec, bld.lm), bld.def(s1, scc), def.getTemp());
          }
 
-         /* Update all other exec masks. */
-         if (nested_cf) {
-            const unsigned global_idx = state == WQM ? 1 : 0;
-            for (unsigned i = global_idx + 1; i < info.exec.size() - 1; i++) {
-               info.exec[i].op = bld.sop2(Builder::s_and, bld.def(bld.lm), bld.def(s1, scc),
-                                          info.exec[i].op, Operand(exec, bld.lm));
-            }
-            /* Update current exec and save WQM mask. */
-            info.exec[global_idx].op =
-               bld.sop1(Builder::s_and_saveexec, bld.def(bld.lm), bld.def(s1, scc),
-                        Definition(exec, bld.lm), info.exec.back().op, Operand(exec, bld.lm));
-            info.exec.back().op = Operand(exec, bld.lm);
-         }
          continue;
 
       } else if (instr->opcode == aco_opcode::p_elect) {
